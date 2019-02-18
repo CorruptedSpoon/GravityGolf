48712f8dfa51435791e92c702d2eddbacdcf1f87
Nick Zavattero <nkz4713@ad.rit.edu>
2019-02-18 12:00:41 -0500

    Universe class work in progress

--- GravityGolf/GravityGolf/Universe.cs
+++ GravityGolf/GravityGolf/Universe.cs
@@ -12,33 +12,48 @@ namespace GravityGolf
     class Universe
     {
         List<Planet> planets = new List<Planet>();
-        //Ball ball = new Ball;
-
-        //Gets gravitational force at position pos
-        /*public ForceAt(Point pos)
+        Ball ball;
+        //Gravitational Constant
+        private double G = (6.674f * Math.Pow(10, -11));
+        
+        public Universe()
         {
-            
+
         }
-        */
+        
+        //Gets gravitational force at position pos
+        public Vector2 ForceAt(Point pos)
+        {
+            Vector2 force = new Vector2();
+            foreach(Planet planet in planets)
+            {
+                force.X += planet.X - pos.X;
+                force.Y += planet.Y - pos.Y;
+            }
+            return force;
+        }
 
         void Add(Planet p)
         {
             planets.Add(p);
         }
 
-        void Draw()
+        void Draw(SpriteBatch sb)
         {
-
+            foreach (Planet planet in planets)
+            {//Where are we going to put the texture file? With the object or just load it onto here?
+                sb.Draw(null, new Vector2(planet.X, planet.Y), Color.White);
+            }
         }
 
-        void Update()
+        void Update() //Check win condition, move ball
         {
 
         }
 
         void Clear()
         {
-
+            planets.Clear();
         }
     }
 }
