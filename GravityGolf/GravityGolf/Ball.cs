7a713d11af690992789ef94abcf458cc1ad86436
fdp8383 <fdp8383@rit.edu>
2019-02-18 11:29:10 -0500

    constructor

--- GravityGolf/GravityGolf/Ball.cs
+++ GravityGolf/GravityGolf/Ball.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GravityGolf {
-    class Ball : Game1 {
+    class Ball : GameObject {
+        public Ball(Vector2 center, int radius, float mass, Texture2D texture, Color color) : base(center, radius, mass, texture, color) {
+
+        }
 
     }
 }
