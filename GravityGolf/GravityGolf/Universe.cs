81dff119cb58882c8ff217d8d493f014abee5cae
Nick Zavattero <nkz4713@ad.rit.edu>
2019-02-18 11:16:33 -0500

    Adding Universe Class

---
+++ GravityGolf/GravityGolf/Universe.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GravityGolf
+{
+    class Universe
+    {
+        List<Planet> planets = new List<Planet>();
+        //Ball ball = new Ball;
+
+        //Gets gravitational force at position pos
+        /*public ForceAt(Point pos)
+        {
+            
+        }
+        */
+
+        void Add(Planet p)
+        {
+            planets.Add(p);
+        }
+
+        void Draw()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        void Clear()
+        {
+
+        }
+    }
+}
